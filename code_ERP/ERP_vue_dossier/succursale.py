50300b50f30d4b11ee2fd2cdcfeb9c4c25f8b634
Alek Del Balso <alekdelbalso@gmail.com>
2024-10-21 10:45:45 -0400

    succursale sauf modifier

--- code_ERP/ERP_vue_dossier/succursale.py
+++ code_ERP/ERP_vue_dossier/succursale.py
@@ -1,118 +1,13 @@
+import sys
+import sqlite3
 from PySide6.QtWidgets import (
-    QTableWidget, QTableWidgetItem,QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QSpacerItem, QSizePolicy, QLabel, QGridLayout, QLineEdit, QDialog, QComboBox
+    QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QTableWidget, QComboBox,
+    QTableWidgetItem, QVBoxLayout, QHBoxLayout, QWidget, QGridLayout, QDialog, QMessageBox
 )
 from PySide6.QtCore import Qt
 
-from utils.QListe import QListe
 from ERP_data_base import DatabaseManager
 
-import sqlite3
-
-class QSuccursale(QWidget):
-    def __init__(self, parent):
-        super().__init__()
-
-        self.vue = parent
-
-        # Titre de la fenêtre
-        self.setWindowTitle("Page Succursale")
-
-        # Initialiser le mode à aucun
-        self.current_mode = None
-
-        # Création des boutons
-        back_button = QPushButton("Back")
-        back_button.clicked.connect(self.vue.basculer_vers_gerant_global)
-        ajouter_button = QPushButton("Ajouter")
-        ajouter_button.clicked.connect(self.ajout)
-        modifier_button = QPushButton("Modifier")
-        retirer_button = QPushButton("Retirer")
-
-        # Connecter les boutons à leurs actions respectives
-        modifier_button.clicked.connect(lambda: self.set_mode('modifier'))
-        retirer_button.clicked.connect(lambda: self.set_mode('retirer'))
-
-        # Mise en page principale
-        self.main_layout = QHBoxLayout()  # Utiliser un layout horizontal
-
-        # Mise en page pour tous les boutons
-        button_layout = QVBoxLayout()
-        button_layout.addWidget(back_button, alignment=Qt.AlignTop)  # Bouton Back en haut
-
-        # Espacement pour centrer les autres boutons
-        button_layout.addStretch()  # Ajout d'un étirement pour espacer le bouton Back des autres
-        button_layout.addWidget(ajouter_button)
-        button_layout.addWidget(modifier_button)
-        button_layout.addWidget(retirer_button)
-        button_layout.addStretch()  # Ajout d'un étirement pour espacer le bouton Back des autres
-
-        # Ajouter du padding entre les boutons
-        button_layout.setSpacing(10)  # Espacement entre les boutons
-        button_layout.setAlignment(Qt.AlignLeft)  # Aligner à gauche
-
-        # Ajouter le layout des boutons au layout principal
-        self.main_layout.addLayout(button_layout)
-
-        # Ajout d'un espace flexible pour pousser la QListe à droite
-        self.main_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
-        self.liste_widget = None
-        self.refresh_liste() 
-        self.setLayout(self.main_layout)
-
-    def set_mode(self, mode):
-        self.current_mode = mode
-        for btn in self.findChildren(QPushButton):
-            if btn.text() == "Modifier":
-                btn.setStyleSheet("background-color: lightgray;" if mode != 'modifier' else "background-color: lightblue;")
-            elif btn.text() == "Retirer":
-                btn.setStyleSheet("background-color: lightgray;" if mode != 'retirer' else "background-color: lightcoral;")
-
-    def handle_button_click(self):
-        if self.current_mode == 'modifier':
-            dialog = AddModifyDialog(self)
-            dialog.exec_()
-        elif self.current_mode == 'retirer':
-            print("Mode Retirer activé.")
-            ## retirer dans la base de données
-        else:
-            self.vue.afficher_message("Selectionner", "Veuillez sélectionner un mode")
-        
-    def ajout(self):
-        dialog = AddModifyDialog(self)
-        dialog.exec_()
-        
-    def refresh_liste(self):
-        """Met à jour la liste des succursales."""
-        if self.liste_widget:
-            self.liste_widget.deleteLater()  # Supprime l'ancienne liste
-
-        try:
-            db_manager = DatabaseManager('erp_database.db')
-            query = "SELECT * FROM Succursales"
-            results = db_manager.execute_query(query, ())
-    
-            self.liste_widget = QTableWidget()
-            self.liste_widget.setGeometry(600,600)
-            self.liste_widget.setColumnCount(3)
-            self.liste_widget.setHorizontalHeaderLabels(["Nom", "Adresse", "Code"])
-
-            self.liste_widget.setRowCount(len(results))
-            for i,row in enumerate(results):
-                _nom = row['nom']
-                _adresse = row['adresse']
-                _code = row["code"]
-                
-                self.liste_widget.setItem(i, 0, QTableWidgetItem(_nom))
-                self.liste_widget.setItem(i, 1, QTableWidgetItem(_adresse))
-                self.liste_widget.setItem(i, 2, QTableWidgetItem(_code))
-
-                #nom_button.clicked.connect(self.handle_button_click)
-
-        except sqlite3.Error as e:
-            print(f"Une erreur est survenue : {e}")
-
-        self.main_layout.addWidget(self.liste_widget, alignment=Qt.AlignCenter)
-        
 
 class AddModifyDialog(QDialog):
     def __init__(self, parent):
@@ -121,9 +16,9 @@ class AddModifyDialog(QDialog):
         self.succursale = parent
         
         #CREER UN EMPLOYÉ
-        db_manager = DatabaseManager('erp_database.db')
         try:
             # Vérifier si la table est vide
+            db_manager = DatabaseManager('erp_database.db')
             check_query = "SELECT COUNT(*) FROM Employes"
             result = db_manager.execute_query(check_query)
             count = result[0][0]  # Récupérer le nombre d'employés
@@ -135,6 +30,7 @@ class AddModifyDialog(QDialog):
                 """
                 parameters = ("Dupont", "Jean", "Développeur")  # Remplacez par les valeurs souhaitées
                 rows_affected = db_manager.execute_update(insert_query, parameters)
+
 
                 if rows_affected > 0:
                     print("Un nouvel employé a été ajouté avec succès.")
@@ -151,7 +47,6 @@ class AddModifyDialog(QDialog):
         # Create the layout
         layout = QGridLayout()
 
-
         # prend le nom des tables
         labels = []
         try:
@@ -165,7 +60,6 @@ class AddModifyDialog(QDialog):
         except sqlite3.Error as e:
             print(f"Une erreur est survenue : {e}")
             
-
         self.inputs = {}
 
         for i, label in enumerate(labels):
@@ -225,20 +119,141 @@ class AddModifyDialog(QDialog):
 
             if rows_affected > 0:
                 print("La succursale a été ajouté avec succès.")
+                self.succursale.load_succursale()
             else:
                 print("Aucune ligne n'a été ajoutée.")
 
         except sqlite3.Error as e:
             print(f"Une erreur est survenue : {e}")
         
-        self.succursale.refresh_liste()
         self.close()
         
         
-
-
         
+class QSuccursale(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        
+        #set database
+        self.db_manager = DatabaseManager('erp_database.db')
 
+        # Create the main layout
+        succursale_layout = QGridLayout()
 
+        # Left-side buttons layout (Ajouter, Retirer, Modifier, etc.)
+        add_button = QPushButton("Ajouter")
+        remove_button = QPushButton("Retirer")
+        modify_button = QPushButton("Modifier")
+        back_button = QPushButton("<-")
 
+        button_layout = QVBoxLayout()
+        button_layout.addWidget(back_button)
+        button_layout.addWidget(add_button)
+        button_layout.addWidget(remove_button)
+        button_layout.addWidget(modify_button)
+
+        # Add button layout to the grid
+        succursale_layout.addLayout(button_layout, 0, 0)
+
+        # Title of the inventory
+        title_label = QLabel("Succursales")
+        title_label.setAlignment(Qt.AlignCenter)
+        succursale_layout.addWidget(title_label, 0, 1)
+
+        # Search bar
+        search_label = QLabel("Rechercher :")
+        search_input = QLineEdit()
+
+        search_layout = QHBoxLayout()
+        search_layout.addWidget(search_label)
+        search_layout.addWidget(search_input)
+        succursale_layout.addLayout(search_layout, 1, 1)
+
+        self.succursale_table = QTableWidget()
+        self.succursale_table.setColumnCount(7)
+        self.setLayout(succursale_layout)
+        self.load_succursale()
+
+        # Add table to layout
+        succursale_layout.addWidget(self.succursale_table, 2, 1)
+
+        # Set central widget
+    
+
+        # Connect button actions to methods
+        add_button.clicked.connect(self.add_item)
+        remove_button.clicked.connect(self.remove_item)
+        modify_button.clicked.connect(self.modify_item)
+        back_button.clicked.connect(parent.basculer_vers_gerant_global)
+             
+    def load_succursale(self):
+        try:
+            db_manager = DatabaseManager('erp_database.db')
+            query = "SELECT nom, adresse, code, gerant, statut, telephone, date_ouverture FROM Succursales"
+            rows = db_manager.execute_query(query, ())
+            self.succursale_table.setHorizontalHeaderLabels(["Nom", "Adresse", "Code", "Gerant", "Statut", "Telephone", "Date d'Ouverture"])
+
+            self.succursale_table.setRowCount(len(rows))
+            for row_index, row_data in enumerate(rows):
+                for col_index, data in enumerate(row_data):
+                    self.succursale_table.setItem(row_index, col_index, QTableWidgetItem(str(data)))
+
+        except sqlite3.Error as e:
+            print(f"Une erreur est survenue : {e}")
+
+    def add_item(self):
+        # Open the Add/Modify dialog
+        dialog = AddModifyDialog(self)
+        dialog.exec_()
+        
+    def remove_item(self):
+        # Activer la sélection dans le tableau
+        self.succursale_table.setSelectionBehavior(QTableWidget.SelectRows)
+        self.succursale_table.setSelectionMode(QTableWidget.SingleSelection)
+        
+        # Connecter l'événement de clic à la méthode `confirm_deletion`
+        self.succursale_table.itemClicked.connect(self.confirm_deletion)
+
+    def confirm_deletion(self, item):
+        """Demander confirmation avant de supprimer un élément."""
+        # Obtenir la ligne de l'élément sélectionné
+        row = item.row()
+
+        nom_succursale = self.succursale_table.item(row, 0).text()
+        code_succursale = self.succursale_table.item(row, 2).text()
+
+        # Boîte de dialogue de confirmation
+        confirmation_dialog = QMessageBox()
+        confirmation_dialog.setWindowTitle("Confirmer la suppression")
+        confirmation_dialog.setText(f"Voulez-vous vraiment supprimer la succursale '{nom_succursale}' (Code: {code_succursale}) ?")
+        confirmation_dialog.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        confirmation_dialog.setIcon(QMessageBox.Warning)
+
+        # Si l'utilisateur confirme la suppression
+        if confirmation_dialog.exec_() == QMessageBox.Yes:
+            self.delete_succursale(code_succursale)
+        else:
+            # Annuler la sélection si l'utilisateur ne veut pas supprimer
+            self.succursale_table.clearSelection()
+
+        # Désactiver la connexion à l'événement après la suppression ou l'annulation
+        self.succursale_table.itemClicked.disconnect()
+        self.load_succursale()
+
+    def delete_succursale(self, code_succursale):
+        try:
+            db_manager = DatabaseManager('erp_database.db')
+            query = "DELETE FROM Succursales WHERE code = ?"
+            db_manager.execute_query(query, (code_succursale,))
+
+            # Message de confirmation
+            print(f"La succursale avec le code {code_succursale} a été supprimé.")
+        except Exception as e:
+            print(f"Erreur lors de la suppression de la succursale: {e}") 
+
+    def modify_item(self):
+        # Code to modify the selected item in the stock
+        print("Modify item clicked")
+        # Add logic to modify the selected row
+        
 
